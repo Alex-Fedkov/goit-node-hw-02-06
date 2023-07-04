c8d716bb4bbdcaa909b84de20c0db821cb3037d0
Alex-Fedkov <107791953+Alex-Fedkov@users.noreply.github.com>
2023-07-04 21:47:22 +0300

    hw4

--- server.js
+++ server.js
@@ -1,53 +1,18 @@
-const express = require("express");
-const cors = require("cors");
+const app = require("./app");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-require("dotenv").config();
+dotenv.config({ path: "./.env" });
 
-const app = express();
-
-// parse application/json
-app.use(express.json());
-// cors
-app.use(cors());
-
-const routerApi = require("./api");
-app.use("/api", routerApi);
-
-app.use((_, res, __) => {
-  res.status(404).json({
-    status: "error",
-    code: 404,
-    message: "Use api on routes: /api/tasks",
-    data: "Not found",
-  });
-});
-
-app.use((err, _, res, __) => {
-  console.log(err.stack);
-  res.status(500).json({
-    status: "fail",
-    code: 500,
-    message: err.message,
-    data: "Internal Server Error",
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-const uriDb = process.env.MONGO_URL;
-
-mongoose.Promise = global.Promise;
-
-const connection = mongoose.connect(uriDb, {
-  useUnifiedTopology: true,
-});
-
-connection
+mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => {
-    app.listen(PORT, function () {
-      console.log(`Server running. Use our API on port: ${PORT}`);
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
     });
+    console.log("Database connection successful");
   })
-  .catch((err) =>
-    console.log(`Server not running. Error message: ${err.message}`)
-  );
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
